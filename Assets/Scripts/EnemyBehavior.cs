d44dafdd6aefb50567cd4e0951c1e06642f55c05
Gabriel Oliver <gabriel-a-oliver@users.noreply.github.com>
2020-04-29 19:48:17 -0700

    added psudocode for respawning and get collisions. Added some code for tracking waypoints

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -7,13 +7,16 @@ public class EnemyBehavior : MonoBehaviour {
 
 	///////Gabe Code//////////////////////////////////////////
 	private bool targetRandomWaypoint = false;
-	private char targetWayPoint;
+	//private char targetWayPoint;
+	public Enums.Directions useSide = Enums.Directions.Up;
+	private GameObject currWayPoint;
+	
 	/////////////////////////////////////////////////
 		
 	// Use this for initialization
 	void Start () {
 		NewDirection();
-		targetWayPoint = chooseRandomWaypoint();
+		/*targetWayPoint = */chooseRandomWaypoint();
 	}
 	
 	// Update is called once per frame
@@ -47,6 +50,14 @@ public class EnemyBehavior : MonoBehaviour {
 		}	
 	}
 
+	////Gabe Code///////////////////////////////////
+	void FixedUpdate () {
+		Utils.SetAxisTowards(useSide, transform, targetWayPoint.position - transform.position);
+
+
+	}
+	////////////////////////////////////////////////
+
 	// New direction will be something completely random!
 	private void NewDirection() {
 		Vector2 v = Random.insideUnitCircle;
@@ -54,10 +65,53 @@ public class EnemyBehavior : MonoBehaviour {
 	}
 
 	//////Gabe Code//////////////////////////////////////////
-	private char chooseRandomWaypoint() {
-		int randomNumber = (int) Random.Range(65f, 70f);
-		char waypointChar = (char) randomNumber;
-		return waypointChar;
+	private /*char*/ void chooseRandomWaypoint() {
+		int randomNumber = (int) Random.Range(1f, 6f);
+		if (randomNumber == 1) {
+			currWayPoint = Instantiate(Resources.load("Prefabs/WaypointA"));
+		} else if (randomNumber == 2) {
+			currWayPoint = Instantiate(Resources.load("Prefabs/WaypointB"));
+		} else if (randomNumber == 3) {
+			currWayPoint = Instantiate(Resources.load("Prefabs/WaypointC"));
+		} else if (randomNumber == 4) {
+			currWayPoint = Instantiate(Resources.load("Prefabs/WaypointD"));
+		} else if (randomNumber == 5) {
+			currWayPoint = Instantiate(Resources.load("Prefabs/WaypointE"));
+		} else { //if (randomNumber == 6)
+			currWayPoint = Instantiate(Resources.load("Prefabs/WaypointF"));
+		}
+		//char waypointChar = (char) randomNumber;
+		//return waypointChar;
 	}
 	////////////////////////////////////////////////
 }
+
+/*
+on collision with a waypoint
+
+if (targetRandomWaypoint) {
+	chooseRandomWaypoint();
+} else {
+	if (currWayPoint == Instantiate(Resources.load("Prefabs/WaypointE"))) {
+		currWayPoint = Instantiate(Resources.load("Prefabs/WaypointE"))
+	}
+}
+
+
+*/
+
+/*
+on collision with player
+
+increment "touched enemy" counter
+teleport this object to a random location
+
+*/
+
+/*
+on collision with bullet
+
+increment "destroyed enemy" counter
+teleport this object to a random location
+
+*/
