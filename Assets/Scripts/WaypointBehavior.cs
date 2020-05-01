8459f06073312bb6877b22458f1e838f1e8d264a
johnnykvp <johnnyp1008@gmail.com>
2020-05-01 00:44:26 -0700

    Smoother H key to hide waypoints toggle

--- Assets/Scripts/WaypointBehavior.cs
+++ Assets/Scripts/WaypointBehavior.cs
@@ -11,7 +11,7 @@ public class WaypointBehavior : MonoBehaviour
         sprite = GetComponent<SpriteRenderer>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.H))
         {
